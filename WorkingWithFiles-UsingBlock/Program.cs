bb9d523fb8960292de9b9789e496526430fd74bc
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-21 00:06:05 -0300

    Working with Files, especifically with the 'using'

---
+++ WorkingWithFiles-UsingBlock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFiles_UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            try { 
+            //using (FileStream fs = new FileStream(path, FileMode.Open))
+            //{
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                //}
+            }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
